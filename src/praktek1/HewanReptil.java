c1dc1c3b7d5f52e02249ba6fbd6457625bff545d
Dedyset <dedy.smkn2bjb@gmail.com>
2018-03-27 20:12:42 +0800

    Membuat Atribut dan Behavior

--- src/praktek1/HewanReptil.java
+++ src/praktek1/HewanReptil.java
@@ -10,5 +10,17 @@ package praktek1;
  * @author USER
  */
 public class HewanReptil {
-    
+   String Habitat;
+   String Warna;
+   String Jenis_makanan;
+   String Berbisa;
+   String Bersisik;
+   
+   void cetakInfo(){
+       System.out.println("Habitat          :"+Habitat);
+       System.out.println("Warna            :"+Warna);
+       System.out.println("Jenis_makanan    :"+Jenis_makanan);
+       System.out.println("Berbisa          :"+Berbisa);
+       System.out.println("Bersisik         :"+Bersisik);
+   }
 }
