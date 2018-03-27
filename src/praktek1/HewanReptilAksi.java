10c42d9aa12fdb963534d9edc4c9ba788ed76470
Dedyset <dedy.smkn2bjb@gmail.com>
2018-03-27 20:18:49 +0800

    Mengisi nilai atribut

--- src/praktek1/HewanReptilAksi.java
+++ src/praktek1/HewanReptilAksi.java
@@ -12,5 +12,27 @@ package praktek1;
 public class HewanReptilAksi {
     public static void main(String[] args) {
         HewanReptil Cobra = new HewanReptil();
+         Cobra.Habitat="Lingkungan hutan tropika";
+        Cobra.Warna="Dari hitam atau coklat tua sampai putih-kuning";
+        Cobra.Jenis_makanan="Karnivora";
+        Cobra.Berbisa="Ya";
+        Cobra.Bersisik="Ya";
+        
+        HewanReptil Buaya = new HewanReptil();
+        
+        Buaya.Habitat="Perairan tawar seperti sungai, danau, rawa";
+        Buaya.Warna="Cokelat, putih, hitam";
+        Buaya.Jenis_makanan="Karnivora";
+        Buaya.Berbisa="Tidak";
+        Buaya.Bersisik="Tidak";
+        
+        HewanReptil Kadal = new HewanReptil();
+        
+        Kadal.Habitat="Hidup dipohon dan rerumputan";
+        Kadal.Warna="Cokelat, hijau";
+        Kadal.Jenis_makanan="Karnivora";
+        Kadal.Berbisa="Ya";
+        Kadal.Bersisik="Tidak";
+        
     }
 }
